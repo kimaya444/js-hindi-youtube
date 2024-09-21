09aaaa63d8b11166ef4386c9749316f0636f3481
kimaya444 <kimayapatil444@gmail.com>
2024-09-21 20:24:40 +0000

    data_types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3+3)
+
+console.log("kimaya")
+
+let name = "hitesh" // string data type
+let age = 18  // number data type
+let isLoggedIn = false // boolean data type
+
+//number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => let state; 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
